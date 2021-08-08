c5f70be24019a970d1ba6f337a7d137f1ad328f9
king <king@qq.com>
2021-08-08 13:38:24 +0800

    hot-fix commit

--- src/main/java/com/atguigu/gmall/service/impl/UserServiceImpl.java
+++ src/main/java/com/atguigu/gmall/service/impl/UserServiceImpl.java
@@ -17,7 +17,7 @@ public class UserServiceImpl implements UserService {
 	@Override
 	public List<UserAddress> getUserAddressList(String userId) {
 		System.out.println("UserServiceImpl.....old...");
-		System.out.println("啊哈哈2");
+		System.out.println("啊哈哈2哈哈哈");
 		// TODO Auto-generated method stub
 		UserAddress address1 = new UserAddress(1, "北京市昌平区宏福科技园综合楼3层", "1", "李老师", "010-56253825", "Y");
 		UserAddress address2 = new UserAddress(2, "深圳市宝安区西部硅谷大厦B座3层（深圳分校）", "1", "王老师", "010-56253825", "N");
